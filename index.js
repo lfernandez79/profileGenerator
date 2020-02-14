a6b067000f146bb837d987fe2d346471b6206071
LeoFdz <lfernandez79@github.com>
2020-02-13 22:05:14 -0600

    added asyc/await = it doesnt work

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const axios = require("axios");
 
 const writeFileAsync = util.promisify(fs.writeFile); 
 
-function userQuestions() {
+
+const getInfo = async function userQuestions() {
+    
     return inquirer.prompt([
         {
             type: "input",
@@ -24,10 +26,23 @@ function userQuestions() {
         },
         {
             type: "input",
-            message: "What's respository name?",
+            message: "What is the respository name?",
             name: "title"
         },
     ])
-};
-console.log();
+    
+    try {
+        const { name, email, username, title } = await inquirer.prompt(questions);
+
+        let { data } = await axios.get(`https://api.github.com/users/${username}`)
+
+    } catch (err) {
+        console.log(err);
+    }
+ };
+
+console.log(getInfo);
 // https://api.github.com/users/lfernandez79
+
+
+
