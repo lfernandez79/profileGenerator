6213f9a54aeec790a3664f33d91f197e73d7587f
LeoFdz <lfernandez79@github.com>
2020-02-13 21:32:03 -0600

    modified quesiton in index.js also .json files

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
-const inquirer = requirer ("inquiere");
-const fs = requierer ("fs");
+const inquirer = require ("inquirer");
+const fs = require ("fs");
 const util = require("util");
+const axios = require("axios");
 
 const writeFileAsync = util.promisify(fs.writeFile); 
 
-function promptUser() {
+function userQuestions() {
     return inquirer.prompt([
         {
             type: "input",
-            name: "name",
-            message: "What is your name?"
-        }
-    ]);
-}
+            message: "What is your name?",
+            name: "name"
+        },
+        {
+            type: "input",
+            message: "What is your email?",
+            name: "email"
+        },
+        {
+            type: "input",
+            message: "What is your github username?",
+            name: "username"
+        },
+        {
+            type: "input",
+            message: "What's respository name?",
+            name: "title"
+        },
+    ])
+};
+console.log();
+// https://api.github.com/users/lfernandez79
