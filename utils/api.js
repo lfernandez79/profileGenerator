dc9d250d4be321dca76ad91aefc1d606d7c4d22f
LeoFdz <lfernandez79@github.com>
2020-02-14 20:32:40 -0600

    api file minor update

--- utils/api.js
+++ utils/api.js
@@ -1,7 +1,16 @@
+const axios = require("axios");
+require("dotenv").config();
+
 const api = {
   getUser(username) {
-
-  }
+// GRAB USER INFROMATION
+    const queryUrl = `https://api.github.com/users/${username}`;
+    console.log(queryUrl);
+    return axios.get(queryUrl).catch(err => {
+      console.log("User not found");
+      process.exit(1);
+    });
+  },
 };
 
 module.exports = api;
